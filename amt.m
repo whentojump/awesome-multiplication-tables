3e244b16fb0c9b7fb9b0b316e21cf802104bb246
wtj <35722712+whentojump@users.noreply.github.com>
2022-12-22 19:01:35 +0800

    suppress MATLAB warnings

--- amt.m
+++ amt.m
@@ -3,8 +3,8 @@
 % GNU Octave tested, but the "env -S" feature in shebang requires at least coreutils 8.30. This part not tested. TODO
 % MATLAB seems very unhappy with this shebang. Lol.
 
-for i = [1:9]
-    for j = [1:9]
+for i = 1:9
+    for j = 1:9
         fprintf('%d * %d = %d\t', i, j, i*j);
     end
     fprintf('\n');
