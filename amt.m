b788a413899bf183722bb0be3d0f69ef9be9cff1
wtj <35722712+whentojump@users.noreply.github.com>
2022-12-22 18:59:33 +0800

    TIL a useless fact

--- amt.m
+++ amt.m
@@ -1,7 +1,7 @@
 #! /usr/bin/env -S octave --no-window-system
 
 % GNU Octave tested, but the "env -S" feature in shebang requires at least coreutils 8.30. This part not tested. TODO
-% MATLAB not tested. TODO
+% MATLAB seems very unhappy with this shebang. Lol.
 
 for i = [1:9]
     for j = [1:9]
